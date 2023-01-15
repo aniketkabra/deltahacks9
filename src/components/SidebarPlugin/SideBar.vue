7772aff9f60181c06e76a7b251cd9e87bfdd4715
Isra Zahid <zahidi1@mcmaster.ca>
2023-01-14 20:18:03 -0500

    Sidebar initial

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -1,34 +1,12 @@
 <template>
   <div class="sidebar"
        :style="sidebarStyle">
-    <div class="sidebar-wrapper">
-      <div class="logo">
-        <a href="#" class="simple-text logo__container">
-            <div class="logo-img">
-                <img src="img/vue-logo.png" alt="">
-            </div>
-          {{title}}
-        </a>
+      <div class="sidebar-wrapper">
+        <div class="introduction">
+          <h5><b>Good Morning,</b></h5>
+          <p>{{title}}</p>
+        </div>
       </div>
-
-      <slot name="content"></slot>
-      <ul class="nav nav-main__links">
-        <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
-        <slot>
-          <sidebar-link v-for="(link,index) in sidebarLinks"
-                        :key="link.name + index"
-                        :to="link.path"
-                        @click="closeNavbar"
-                        :link="link">
-            <i :class="link.icon"></i>
-            <p>{{link.name}}</p>
-          </sidebar-link>
-        </slot>
-      </ul>
-      <ul class="nav nav-bottom" v-if="$slots['bottom-links']">
-        <slot name="bottom-links"></slot>
-      </ul>
-    </div>
   </div>
 </template>
 <script>
@@ -41,23 +19,7 @@
     props: {
       title: {
         type: String,
-        default: 'Vue LBD'
-      },
-      backgroundColor: {
-        type: String,
-        default: 'black',
-        validator: (value) => {
-          let acceptedValues = ['', 'blue', 'azure', 'green', 'orange', 'red', 'purple', 'black']
-          return acceptedValues.indexOf(value) !== -1
-        }
-      },
-      activeColor: {
-        type: String,
-        default: 'success',
-        validator: (value) => {
-          let acceptedValues = ['primary', 'info', 'success', 'warning', 'danger']
-          return acceptedValues.indexOf(value) !== -1
-        }
+        default: 'Dr. Heinz Doofenshmirtz'
       },
       sidebarLinks: {
         type: Array,
